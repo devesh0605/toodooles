62c614aebb7fc18da4df7b21cbbd42db44c023b1
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-27 21:48:16 +0530

    project initiated

--- App.js
+++ App.js
@@ -4,30 +4,11 @@ import {StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 
 export default function App() {
   
-  const [people,setPeople]=useState([
-    {pname: 'chandler',id:'1'},
-    {pname: 'monica',id:'2'},
-    {pname: 'ross',id:'3'},
-    {pname: 'raechal',id:'4'},
-    {pname:  'joey',id:'5'},
-    {pname: 'phebe',id:'6'},
-  ])
+ 
+
   return (
     <View style={styles.container}>
-      <FlatList
-      numColumns={2}
-      keyExtractor={(item)=>item.id}
-      data = {people}
-      renderItem ={({item})=>(
-        <TouchableOpacity>
-        <Text style={styles.item}>{item.pname}</Text>
-        </TouchableOpacity>
-      )}
-      />
-      
-      
-      
-    
+     
       <StatusBar style="auto" />
       
     </View>
@@ -42,13 +23,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   
-item : {
-    marginTop : 24,
-    padding:30,
-    backgroundColor:'green',
-    fontSize:30,
-    marginHorizontal:10,
-    marginHorizontal:24,
 
-  },
 });
