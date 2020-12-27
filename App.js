1c17edf3e8b7a91213bd204867119afce03d3732
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-27 18:21:51 +0530

    added buttons

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name,setName]=useState('Tom Riddle')
+  const [person, setPerson]=useState({name:'Snape', age:32})
+  const clickHandler = () => {
+    setName('Voldemort')
+    setPerson({name:'Harry',age:'11'})
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -14,6 +20,11 @@ export default function App() {
       <Text >Lorem ipsum <Text style={styles.boldText}>Luke harper</Text> dolor sit amet</Text>
       <Text>Lorem ipsum dolor sit amet</Text>
       <Text>Lorem ipsum dolor sit amet</Text>
+      </View>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}.</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Updte me' onPress={clickHandler}/>
       </View>
       
       <StatusBar style="auto" />
@@ -38,5 +49,8 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: 'yellow',
     padding: 20,
+  },
+  buttonContainer : {
+    marginTop : 20,
   }
 });
