dac0b2c7df329da4da29d28d013714e15d93e31f
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-27 17:57:06 +0530

    basic text styles

--- App.js
+++ App.js
@@ -5,7 +5,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to end on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>
+          Hello World
+        </Text>
+      </View>
+      <View style={styles.body}>
+      <Text >Lorem ipsum <Text style={styles.boldText}>Luke harper</Text> dolor sit amet</Text>
+      <Text>Lorem ipsum dolor sit amet</Text>
+      <Text>Lorem ipsum dolor sit amet</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +28,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header : {
+    backgroundColor:'pink',
+    padding :20,
+  },
+  boldText: {
+    fontWeight :'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
